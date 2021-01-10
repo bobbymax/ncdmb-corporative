e6a65f0d33f5e7d2a8cd2db0f39d0cb5a8060d58
Joshua Osagie <joshxtreme@raaxo.ng>
2021-01-10 12:08:03 +0100

    force json response

--- routes/api.php
+++ routes/api.php
@@ -18,17 +18,20 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('members', 'MemberController');
-Route::apiResource('budgets', 'BudgetController');
-Route::apiResource('expenditures', 'ExpenditureController');
-Route::apiResource('categories', 'CategoryController');
-Route::apiResource('groups', 'GroupController');
-Route::apiResource('guarantors', 'GuarantorController');
-Route::apiResource('investments', 'InvestmentController');
-Route::apiResource('loans', 'LoanController');
-Route::apiResource('roles', 'RoleController');
-Route::apiResource('schedules', 'ScheduleController');
-Route::apiResource('permissions', 'PermissionController');
-Route::apiResource('services', 'ServiceController');
-Route::apiResource('specifications', 'SpecificationController');
-Route::apiResource('contributions', 'ContributionController');
+// force json response
+Route::group(['middleware' => ['jsonable']], function () {
+    Route::apiResource('members', 'MemberController');
+    Route::apiResource('budgets', 'BudgetController');
+    Route::apiResource('expenditures', 'ExpenditureController');
+    Route::apiResource('categories', 'CategoryController');
+    Route::apiResource('groups', 'GroupController');
+    Route::apiResource('guarantors', 'GuarantorController');
+    Route::apiResource('investments', 'InvestmentController');
+    Route::apiResource('loans', 'LoanController');
+    Route::apiResource('roles', 'RoleController');
+    Route::apiResource('schedules', 'ScheduleController');
+    Route::apiResource('permissions', 'PermissionController');
+    Route::apiResource('services', 'ServiceController');
+    Route::apiResource('specifications', 'SpecificationController');
+    Route::apiResource('contributions', 'ContributionController');
+});
