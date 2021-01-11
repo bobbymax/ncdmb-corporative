fad3f0374142cc11b093ab5773e2863907d231aa
Joshua Osagie <joshxtreme@raaxo.ng>
2021-01-11 22:52:12 +0100

    more factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,8 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-        // \App\Models\Member::factory(1)->create();
+        \App\Models\User::factory(10)->create();
+        \App\Models\Member::factory(1)->create();
         \App\Models\Budget::factory(5)->create();
         \App\Models\Category::factory(5)->create();
         \App\Models\Contribution::factory(5)->create();
