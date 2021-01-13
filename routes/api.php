c6d8b1002adac638ae3251195a990a57c7f74d59
Joshua Osagie <joshxtreme@raaxo.ng>
2021-01-13 11:53:17 +0100

    cors middleware

--- routes/api.php
+++ routes/api.php
@@ -9,21 +9,23 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('members', 'MemberController');
-Route::apiResource('budgets', 'BudgetController');
-Route::apiResource('expenditures', 'ExpenditureController');
-Route::apiResource('categories', 'CategoryController');
-Route::apiResource('groups', 'GroupController');
-Route::apiResource('guarantors', 'GuarantorController');
-Route::apiResource('investments', 'InvestmentController');
-Route::apiResource('loans', 'LoanController');
-Route::apiResource('roles', 'RoleController');
-Route::apiResource('schedules', 'ScheduleController');
-Route::apiResource('permissions', 'PermissionController');
-Route::apiResource('services', 'ServiceController');
-Route::apiResource('specifications', 'SpecificationController');
-Route::apiResource('contributions', 'ContributionController');
-Route::apiResource('transactions', 'TransactionController');
-Route::apiResource('wallets', 'WalletController');
+Route::group(['middleware' => ['cors']], function () {
+    Route::apiResource('members', 'MemberController');
+    Route::apiResource('budgets', 'BudgetController');
+    Route::apiResource('expenditures', 'ExpenditureController');
+    Route::apiResource('categories', 'CategoryController');
+    Route::apiResource('groups', 'GroupController');
+    Route::apiResource('guarantors', 'GuarantorController');
+    Route::apiResource('investments', 'InvestmentController');
+    Route::apiResource('loans', 'LoanController');
+    Route::apiResource('roles', 'RoleController');
+    Route::apiResource('schedules', 'ScheduleController');
+    Route::apiResource('permissions', 'PermissionController');
+    Route::apiResource('services', 'ServiceController');
+    Route::apiResource('specifications', 'SpecificationController');
+    Route::apiResource('c', 'ContributionController');
+    Route::apiResource('transactions', 'TransactionController');
+    Route::apiResource('wallets', 'WalletController');
+});
 
-Route::post('login', [LoginController::class,'login']);
+Route::post('login', [LoginController::class, 'login']);
