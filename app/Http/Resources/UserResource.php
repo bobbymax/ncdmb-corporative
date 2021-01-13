314c6474f0c4e08b485128d778f82906904d6621
Joshua Osagie <joshxtreme@raaxo.ng>
2021-01-13 20:23:54 +0100

    user resource

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -30,8 +30,8 @@ class UserResource extends JsonResource
             'date_joined' => $this->date_joined->format('d M, Y'),
             'mobile' => $this->mobile,
             'contribution' => $this->contribution,//->only('fee'),
-            'next of kin' => $this->kin//->only('name', 'relationship', 'mobile'),
-            // 'wallet' => $this->wallet->only(['identifier', 'current', 'available', 'ledger']),
+            'next of kin' => $this->kin,//->only('name', 'relationship', 'mobile'),
+            'wallet' => $this->wallet//->only(['identifier', 'current', 'available', 'ledger']),
             // 'roles' => RoleResource::collection($this->roles),
         ];
     }
