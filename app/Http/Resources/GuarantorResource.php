5b1e1f8e10a0ac4299e11314b88a6aae07224310
Joshua Osagie <joshxtreme@raaxo.ng>
2021-01-25 21:32:05 +0100

    change: guarantor resource

--- app/Http/Resources/GuarantorResource.php
+++ app/Http/Resources/GuarantorResource.php
@@ -19,7 +19,7 @@ class GuarantorResource extends JsonResource
     {
         // return parent::toArray($request);
         $loan_code = Loan::where('code', $this->code)->get('id');
-        return Loan::where('code', $this->code);
+        // return gettype($loan_code[0]['id']);
         $guarantors = Guarantor::where('loan_id', $loan_code[0]['id'])->get('user_id');
         $users = "";
         $arr = collect([]);
