9d43bc69dd34dd1c1b2b216e3a85e1561715fe62
Joshua Osagie <joshxtreme@raaxo.ng>
2021-01-25 23:04:33 +0100

    loan resource

--- app/Http/Resources/LoanResource.php
+++ app/Http/Resources/LoanResource.php
@@ -25,7 +25,7 @@ class LoanResource extends JsonResource
             'description' => $this->description,
             'frequency' => $this->frequency,
             'status' => $this->status,
-            'member' => $this->member,
+            'member' => $this,
             'category' => $this->category,
             'created_at' => $this->created_at->format('d M, Y'),
             'closed' => $this->closed == 1 ? true : false,
