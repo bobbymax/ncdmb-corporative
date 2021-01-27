542ba35674da87191e601c166c8ca8f7e9ddc53e
Joshua Osagie <joshxtreme@raaxo.ng>
2021-01-27 01:11:46 +0100

    feature: working on approval

--- config/database.php
+++ config/database.php
@@ -54,8 +54,8 @@ return [
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
-            // 'charset' => 'utf8mb4', // local url
-            'charset' => 'utf8', //heroku
+            'charset' => 'utf8mb4', // local url
+            // 'charset' => 'utf8', //heroku
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
             'prefix_indexes' => true,
