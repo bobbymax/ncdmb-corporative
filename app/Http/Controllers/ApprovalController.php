b36e898e1f9b87e286ef583b76272366ef3cdb15
Joshua Osagie <joshxtreme@raaxo.ng>
2021-01-27 11:54:26 +0100

    approval accept/reject

--- app/Http/Controllers/ApprovalController.php
+++ app/Http/Controllers/ApprovalController.php
@@ -28,7 +28,7 @@ class ApprovalController extends Controller
         $loan_id = Guarantor::where([
             ['user_id', $request->user()->id],
             ['status', 'pending']
-        ])->orWhere('status', 'denied')->get('loan_id');
+        ])->get('loan_id');
         return response()->json(
             ApprovalResource::collection(Loan::find($loan_id->pluck('loan_id')))
         );
