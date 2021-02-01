66e0215d9abcc191cd48a9f7c5ca025792c245a3
Josh Xtreme <joshxtreme@raaxo.ng>
2021-02-01 17:05:12 +0100

    transaction

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -18,17 +18,21 @@ class DashboardController extends Controller
     public function index()
     {
         // $roles = $request->user()->roles;
-        $deposits = Transaction::where('type', 'deposit')->get();
+        $deposits_online = Transaction::where('type', 'online')->get()->pluck('amount')->sum();
+        $deposits_bank = Transaction::where('type', 'bank')->get()->pluck('amount')->sum();
+        $deposits = $deposits_online + $deposits_bank;
         $contributions = Transaction::where('type', 'contribution')->get();
         $loans = Transaction::where('type', 'loan')->get();
         $withdrawals = Transaction::where('type', 'withdrawal')->get();
+        $availBalance = Transaction::all()->pluck('amount')->sum();
 
         return response()->json(
             [
-                'totalDeposits' => $deposits->pluck('amount')->sum(),
+                'totalDeposits' => $deposits,//->pluck('amount')->sum(),
                 'totalContributions' => $contributions->pluck('amount')->sum(),
                 'totalLoans' => $loans->pluck('amount')->sum(),
-                'totalWithdrawals' => 0//$withdrawals,
+                'totalWithdrawals' => $withdrawals->pluck('amount')->sum(),
+                'availableBalance' => $availBalance
             ]
         );
     }
@@ -38,15 +42,19 @@ class DashboardController extends Controller
         $transactee_id = Transactee::where('user_id', $request->user()->id)
             ->where('status', 'receiver')->get('transaction_id')->first()->transaction_id;
 
-        $transaction_query = Transaction::where('id', $transactee_id)->where('type', 'deposit');
+        $deposit_query = Transaction::where('id', $transactee_id)->where('type', 'deposit');
 
         $contribution_query = Transaction::where('id', $transactee_id)->where('type', 'contribution');
 
         $loan_query = Transaction::where('id', $transactee_id)->where('type', 'loan');
 
+        $withdrawal_query = Transaction::where('id', $transactee_id)->where('type', 'withdrawal');
+
+        $available_bal_amt = Transaction::where('id', $transactee_id)->pluck('amount')->sum();
+
         $deposit_amt =
-            $transaction_query->first() !== null
-            ? $transaction_query
+            $deposit_query->first() !== null
+            ? $deposit_query
             ->first()->amount
             : 0;
 
@@ -62,13 +70,20 @@ class DashboardController extends Controller
             ->first()->amount
             : 0;
 
+        $withdrawal_amt =
+            $withdrawal_query->first() !== null
+            ? $withdrawal_query
+            ->first()->amount
+            : 0;
+
         return response()->json(
             [
                 'totalDeposits' => $deposit_amt,
                 'totalContributions' => $contribution_amt,
                 'totalLoans' => $loan_amt,
-                'totalWithdrawals' => 0,//$withdrawal_amt,
-                'currentLoan' => 0//$withdrawal_amt,
+                'totalWithdrawals' => $withdrawal_amt,
+                'currentLoan' => 0, //$withdrawal_amt,
+                'availableBalance' => $available_bal_amt //$withdrawal_amt,
                 // 'member'=>new UserResource($request->user())
             ]
         );
