f2d2f234b01b5f530bdd91d50ea8844e84372670
Josh Xtreme <joshxtreme@raaxo.ng>
2021-02-02 11:19:59 +0100

    dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -32,7 +32,14 @@ class DashboardController extends Controller
 
         if (auth()->user()->hasRole(config('corporative.superAdmin'))) {
             return response()->json([
-                'data' => compact('user', 'admin'),
+                // 'data' => compact('user', 'admin'),
+                'totalContributions' => collect([$admin])->pluck('totalContributions')[0],
+                'totalDeposits' => collect([$admin])->pluck('totalDeposits')[0],
+                'availableBalance' => collect([$admin])->pluck('availableBalance')[0],
+                'totalContributions' => collect([$admin])->pluck('totalContributions')[0],
+                'totalWithdrawals' => collect([$admin])->pluck('totalWithdrawals')[0],
+                'totalLoans' => collect([$admin])->pluck('totalLoans')[0],
+                'currentLoan' => collect([$admin])->pluck('currentLoan')[0],
                 'status' => 'success',
                 'message' => 'Fetched successfully!!'
             ], 200);
