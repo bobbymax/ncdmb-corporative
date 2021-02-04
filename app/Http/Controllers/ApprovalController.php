274086504cd198e3d791cf4273327a889347a886
Josh Xtreme <joshxtreme@raaxo.ng>
2021-02-04 17:48:17 +0100

    update

--- app/Http/Controllers/ApprovalController.php
+++ app/Http/Controllers/ApprovalController.php
@@ -30,7 +30,7 @@ class ApprovalController extends Controller
         $loan_id = Guarantor::where([
             ['user_id', $request->user()->id],
             ['status', 'pending']
-        ])->get('guarantorable_id')->last();
+        ])->get('guarantorable_id');
 
         return response()->json(
             ApprovalResource::collection(Loan::find($loan_id->pluck('guarantorable_id')))
