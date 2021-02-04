646d38385a9c0af5aae4b916d5394cb9ab20625d
Josh Xtreme <joshxtreme@raaxo.ng>
2021-02-04 14:06:07 +0100

    feature: allow denial-loan approval

--- app/Http/Controllers/LoanController.php
+++ app/Http/Controllers/LoanController.php
@@ -278,9 +278,19 @@ class LoanController extends Controller
                 ], 422);
             }
 
+            // 
+            if ($loan->level >= 3) {
+                return response()->json([
+                    'data' => null,
+                    'status' => 'error',
+                    'message' => 'This operation cannot be performed'
+                ], 403);
+            }
+            // 
+
             if ($loan->approvals()->save($role->members->first())) {
                 $loan->level += 1;
-                $loan->status = "registered";
+                $loan->status = $request->status; //"registered";
                 $loan->save();
             }
         }
