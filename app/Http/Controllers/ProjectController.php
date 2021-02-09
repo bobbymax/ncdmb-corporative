54555f51fc810cbb39c7f919fcf1388ab084b361
Josh Xtreme <joshxtreme@raaxo.ng>
2021-02-09 16:01:59 +0100

    :bug: fix project list always returning null

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -22,7 +22,7 @@ class ProjectController extends Controller
      */
     public function index()
     {
-        $projects = Project::latest()->get();
+        $projects = Project::all();
 
         if ($projects->count() < 1) {
             return response()->json([
