7d5384f0c9b705866b934dc364b12c40e081360f
Bobby Ekaro <bobby.ekaro@raaxo.ng>
2021-04-01 09:50:17 +0100

    Service Category Update

--- app/Http/Controllers/ServiceCategoryController.php
+++ app/Http/Controllers/ServiceCategoryController.php
@@ -2,11 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Service;
 use App\Models\ServiceCategory;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
 
 class ServiceCategoryController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -14,7 +22,22 @@ class ServiceCategoryController extends Controller
      */
     public function index()
     {
-        //
+        $serviceCategories = ServiceCategory::all();
+
+        if ($serviceCategories->count() < 1) {
+            return response()->json([
+                'data' => null,
+                'message' => 'No data found!',
+                'status' => 'info'
+            ], 200);
+        }
+
+        return response()->json([
+            'data' => $serviceCategories,
+            'message' => 'No data found!',
+            'status' => 'success'
+        ], 200);
+
     }
 
     /**
@@ -35,7 +58,30 @@ class ServiceCategoryController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'description' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'data' => $validator->errors(),
+                'message' => 'Please fix this errors',
+                'status' => 'error'
+            ], 500);
+        }
+
+        $serviceCategory = ServiceCategory::create([
+            'name' => $request->name,
+            'label' => Str::slug($request->name),
+            'description' => $request->description,
+        ]);
+
+        return response()->json([
+            'data' => $serviceCategory,
+            'message' => 'Service category created successfully!',
+            'status' => 'success'
+        ], 201);
     }
 
     /**
@@ -44,9 +90,23 @@ class ServiceCategoryController extends Controller
      * @param  \App\Models\ServiceCategory  $serviceCategory
      * @return \Illuminate\Http\Response
      */
-    public function show(ServiceCategory $serviceCategory)
+    public function show($serviceCategory)
     {
-        //
+        $serviceCategory = ServiceCategory::find($serviceCategory);
+
+        if (! $serviceCategory) {
+            return response()->json([
+                'data' => null,
+                'message' => 'Invalid token entered',
+                'status' => 'error'
+            ], 422);
+        }
+
+        return response()->json([
+            'data' => $serviceCategory,
+            'message' => 'Service category details',
+            'status' => 'success'
+        ], 200);
     }
 
     /**
@@ -55,9 +115,23 @@ class ServiceCategoryController extends Controller
      * @param  \App\Models\ServiceCategory  $serviceCategory
      * @return \Illuminate\Http\Response
      */
-    public function edit(ServiceCategory $serviceCategory)
+    public function edit($serviceCategory)
     {
-        //
+        $serviceCategory = ServiceCategory::find($serviceCategory);
+
+        if (! $serviceCategory) {
+            return response()->json([
+                'data' => null,
+                'message' => 'Invalid token entered',
+                'status' => 'error'
+            ], 422);
+        }
+
+        return response()->json([
+            'data' => $serviceCategory,
+            'message' => 'Service category details',
+            'status' => 'success'
+        ], 200);
     }
 
     /**
@@ -67,9 +141,42 @@ class ServiceCategoryController extends Controller
      * @param  \App\Models\ServiceCategory  $serviceCategory
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, ServiceCategory $serviceCategory)
+    public function update(Request $request, $serviceCategory)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'description' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'data' => $validator->errors(),
+                'message' => 'Please fix this errors',
+                'status' => 'error'
+            ], 500);
+        }
+
+        $serviceCategory = ServiceCategory::find($serviceCategory);
+
+        if (! $serviceCategory) {
+            return response()->json([
+                'data' => null,
+                'message' => 'Invalid token entered',
+                'status' => 'error'
+            ], 422);
+        }
+
+        $serviceCategory->update([
+            'name' => $request->name,
+            'label' => Str::slug($request->name),
+            'description' => $request->description,
+        ]);
+
+        return response()->json([
+            'data' => $serviceCategory,
+            'message' => 'Service category updated successfully!',
+            'status' => 'success'
+        ], 200);
     }
 
     /**
@@ -78,8 +185,24 @@ class ServiceCategoryController extends Controller
      * @param  \App\Models\ServiceCategory  $serviceCategory
      * @return \Illuminate\Http\Response
      */
-    public function destroy(ServiceCategory $serviceCategory)
+    public function destroy($serviceCategory)
     {
-        //
+        $serviceCategory = ServiceCategory::find($serviceCategory);
+
+        if (! $serviceCategory) {
+            return response()->json([
+                'data' => null,
+                'message' => 'Invalid token entered',
+                'status' => 'error'
+            ], 422);
+        }
+
+        $serviceCategory->delete();
+
+        return response()->json([
+            'data' => null,
+            'message' => 'Service category deleted successfully!',
+            'status' => 'success'
+        ], 200);
     }
 }
